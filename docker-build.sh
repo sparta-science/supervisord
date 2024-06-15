226873e8bd624a459992523a2ffde2fcb685ed42
Oleksandr Kukhar <kyxap@kyxap.pro>
2024-06-14 20:31:22 -0400

    chore: add build fixes

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,6 @@
-# build and publish docker image: spartahasura/supervisord:v0.7.3 with multi-arch
+VERSION=v0.7.3-fix
+# build and publish docker image: spartahasura/supervisord:$VERSION with multi-arch
 docker buildx build . -f Dockerfile \
   --platform linux/arm64/v8,linux/amd64 \
-  --tag spartahasura/supervisord:v0.7.3 \
+  --tag spartahasura/supervisord:$VERSION \
   --push
